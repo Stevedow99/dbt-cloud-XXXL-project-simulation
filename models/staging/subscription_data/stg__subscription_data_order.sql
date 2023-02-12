614d79c07b304f83c6ae2ecd132adaf59099f693
Stevedow99 <steve.dowling28@gmail.com>
2023-02-12 14:16:54 -0500

    marketing models

--- models/staging/subscription_data/stg__subscription_data_order.sql
+++
@@ -1,4 +0,0 @@
-  Macro 'macro.codegen.generate_base_model' (macros/generate_base_model.sql) depends on a source named 'subscription_data.order' which was not found or is disabled
-  
-  > in macro generate_base_model (macros/generate_base_model.sql)
-  > called by macro generate_base_model (macros/generate_base_model.sql)
